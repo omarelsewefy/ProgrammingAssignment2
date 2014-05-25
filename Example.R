707034ff028475dd1f2e1d02c911c653944e71e1
omarelsewefy <omarelsewefy@gmail.com>
2014-05-25 14:46:53 +0200

    Update Example.R
    This is the file I used during the development and testing of Both makeCacheMatrix function and the cacheSolve.R function

--- Example.R
+++ Example.R
@@ -1,9 +1,8 @@
 #Example File to Run Functions
-
+source('makeCacheMatrix.R')
+source('cacheSolve.R')
 x <- rbind(c(-1,3,-1),c(0,-6,5),c(-5,-3,1))
-x
-cx <- makeCacheMatrix(x)
-cx
-cx$get()
-cacheSolve(cx)
-cacheSolve(cx)
+Special_Matrix <- makeCacheMatrix(x)
+Special_Matrix$get()
+cacheSolve(Special_Matrix)
+cacheSolve(Special_Matrix)
